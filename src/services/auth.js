29bdbd0c4eadc51bb6491fd87cd2de701df42436
ryomario <ardamomo3@gmail.com>
2022-09-11 22:52:56 +0700

    api: endpoint

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,10 @@
 import { baseApi } from '@/services';
 
 // Auth API endpoint
-const api_ep = import.meta.env.VITE_API_AUTH_ENDPOINT;
+const api_ep = '/api';
 
 // Login API endpoint
-const login_ep = import.meta.env.VITE_API_AUTH_LOGIN_ENDPOINT;
+const login_ep = '/login';
 
 const login = (body) => baseApi.post(`${api_ep + login_ep}`, body);
 

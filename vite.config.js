4e9bd36e1edeb3f3fab51a590ad602f86fe2a4b1
ryomario <ardamomo3@gmail.com>
2022-09-13 15:27:15 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  
   base: process.env.NODE_ENV === "production" ? process.env.VITE_BASE_URL : "/",
 })
